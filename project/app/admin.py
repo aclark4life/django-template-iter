1da6d1b95190b90f13b463b94cb28be506b16215
Alex Clark <aclark@aclark.net>
2017-08-07 15:26:37 -0400

    Update

--- project/app/admin.py
+++ project/app/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import TestModel
 
 # Register your models here.
+
+@admin.register(TestModel)
+class TestModelAdmin(admin.ModelAdmin):
+    """
+    """
