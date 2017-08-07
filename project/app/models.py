d63be028dba3717f0fba0de3694ceefa29fadde2
Alex Clark <aclark@aclark.net>
2017-08-07 15:22:11 -0400

    Update

--- project/app/models.py
+++ project/app/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class TestModel(models.Model):
+    """
+    """
+    field_1 = models.CharField(max_length=300, blank=True, null=True)
+    field_2 = models.CharField(max_length=300, blank=True, null=True)
+    field_3 = models.CharField(max_length=300, blank=True, null=True)
