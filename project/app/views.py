c2002d5f1dc22e9dbeb166619d24749ffdf2df1e
Alex Clark <aclark@aclark.net>
2017-08-07 20:06:51 -0400

    Update

--- project/app/views.py
+++ project/app/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from .models import TestModel
+from collections import OrderedDict
 
 # Create your views here.
 
@@ -10,8 +11,9 @@ def home(request):
     context = {}
     items = TestModel.objects.all()
     for item in items:
-        for field in item._meta._get_fields():
-            item.fields = {}
+        fields = item._meta._get_fields()
+        item.fields = OrderedDict()
+        for field in fields:
             item.fields[field.name] = getattr(item, field.name)
     context['items'] = items
     return render(request, 'base.html', context) 
